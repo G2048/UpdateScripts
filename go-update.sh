b32b3b1005dde8b8f06af64e87cb578fe6b7a1bf
IX <borntokill.ix@gmail.com>
2025-12-29 13:48:34 +0700

    feat(go-update.sh): go install programms

---
+++ go-update.sh
@@ -0,0 +1,11 @@
+go install github.com/jesseduffield/lazydocker@latest
+go install github.com/codesenberg/bombardier@latest
+go install github.com/wagoodman/dive@latest
+# go install github.com/julien040/anyquery@latest
+go install github.com/bastengao/gncdu@latest
+go install github.com/ajayd-san/gomanagedocker@main
+go install github.com/jesseduffield/lazygit@latest
+# curl -fsSL https://raw.githubusercontent.com/GianlucaP106/mynav/main/install.bash | bash
+go install -v github.com/ogen-go/ogen/cmd/ogen@latest
+go install google.golang.org/protobuf/cmd/protoc-gen-go@latest
+go install google.golang.org/grpc/cmd/protoc-gen-go-grpc@latest
